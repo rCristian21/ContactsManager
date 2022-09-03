3425edb049f59f35a960481223b8d51c8eccf9d3
Cristian Ramirez <ccrhdbj@outlook.com>
2022-09-03 14:09:58 -0500

    App Converter

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
 import Counter from './Counter'
+import Converter from "./Converter";
 
-const el = <Counter />; 
+const el = <Converter />; 
 ReactDOM.render(
   el, 
   document.getElementById('root')
