a20e9f9be927865bff092b5da565a46cd4a5fd2d
Cristian Ramirez <ccrhdbj@outlook.com>
2022-09-05 17:30:50 -0500

    Counter with redux separated files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore} from 'redux'
-import { connect } from 'react-redux';
+import { createStore } from 'redux';
+import Counter from './Counter';
 import './style.css';
-
-// Action creator
-function incrementCounter(num) {
-  return { type: 'INCREMENT', num: num }
-}
 
 const initialState = {
   count: 0
@@ -23,48 +18,12 @@ function reducer(state = initialState, action) {
   }
 }
 
-function Counter(props) {
-  function handleClick() {
-    props.incrementCounter(1);
-  }
-    return <div>
-    <p>{props.count}</p>
-    <button onClick={handleClick}>Increment</button>
-    </div>;
-}
-
-function mapStateToProps(state) {
-  return {
-    count: state.count
-  };
-}
-const mapDispatchToProps = {
-  incrementCounter
-}
-
 const store = createStore(reducer);
-
-const Dounter = connect(mapStateToProps, mapDispatchToProps)(Counter);
-
 const el = <Provider store={store}>
-          <Dounter/>
+          <Counter/>
         </Provider>; 
 
 ReactDOM.render(
   el, 
   document.getElementById('root')
 );
-
-
-
-/* import React from "react";
-import ReactDOM from "react-dom";
-import "./style.css";
-import Counter from './Counter'
-import Converter from "./Converter";
-
-const el = <Counter />; 
-ReactDOM.render(
-  el, 
-  document.getElementById('root')
-); */
