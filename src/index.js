7e6e308faba4b5e56a91549d4e8a8c308532d5dd
Cristian Ramirez <ccrhdbj@outlook.com>
2022-09-05 17:14:01 -0500

    Counter with redux, createStore Deprecate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,70 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { legacy_createStore as createStore} from 'redux'
+import { connect } from 'react-redux';
+import './style.css';
+
+// Action creator
+function incrementCounter(num) {
+  return { type: 'INCREMENT', num: num }
+}
+
+const initialState = {
+  count: 0
+};
+// Reducer function
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return { count: state.count + action.num };
+    default:
+      return state;
+  }
+}
+
+function Counter(props) {
+  function handleClick() {
+    props.incrementCounter(1);
+  }
+    return <div>
+    <p>{props.count}</p>
+    <button onClick={handleClick}>Increment</button>
+    </div>;
+}
+
+function mapStateToProps(state) {
+  return {
+    count: state.count
+  };
+}
+const mapDispatchToProps = {
+  incrementCounter
+}
+
+const store = createStore(reducer);
+
+const Dounter = connect(mapStateToProps, mapDispatchToProps)(Counter);
+
+const el = <Provider store={store}>
+          <Dounter/>
+        </Provider>; 
+
+ReactDOM.render(
+  el, 
+  document.getElementById('root')
+);
+
+
+
+/* import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
-import ContactsManager from "./ContactsManager";
+import Counter from './Counter'
+import Converter from "./Converter";
 
-const contacts = ["James Smith", "Thomas Anderson", "Cristian"];
-
-ReactDOM.render(<ContactsManager data={contacts} />, document.getElementById("root"));
+const el = <Counter />; 
+ReactDOM.render(
+  el, 
+  document.getElementById('root')
+); */
